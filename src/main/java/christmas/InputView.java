d4052f2bc7b6003bf6ae69e8cf5815dd02776f6a
DobbyMac <kimdobby@g.skku.edu>
2023-11-14 03:22:26 +0900

    feat - InputView : 원시형태의 날짜와 주문 입력을 Return하는 기능 추가

---
+++ src/main/java/christmas/InputView.java
@@ -0,0 +1,36 @@
+package christmas;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+
+    public static int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        while (true) {
+            try {
+                String inputDate = Console.readLine().trim();
+                InputValidator.validateDateInput(inputDate);
+                return Integer.parseInt(inputDate);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    public static String[] readOrderInputs() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        while (true) {
+            try {
+                String[] orderInputs = Console.readLine().split(",");
+                InputValidator.validateOrderInputs(orderInputs);
+                return orderInputs;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+}
