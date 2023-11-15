b851149d741881fe28427fab7c25cd9f344a4efd
DobbyMac <kimdobby@g.skku.edu>
2023-11-15 13:56:18 +0900

    feat - ErrorMessage : 에러메세지 상수화 및 포매팅 기능 추가

---
+++ src/main/java/christmas/ErrorMessage.java
@@ -0,0 +1,10 @@
+package christmas;
+
+public class ErrorMessage {
+
+    public static final String ERROR_MESSAGE_FORMAT = "[ERROR] 유효하지 않은 {}입니다. 다시 입력해 주세요.";
+
+    public static String formatErrorMessage(String errorType) {
+        return ERROR_MESSAGE_FORMAT.replace("{}", errorType);
+    }
+}
